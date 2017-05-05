8d6d65ad33fc85ba18577afb8a046f2fe7f604d2
Philip Lee <pmoseslee@gmail.com>
2017-05-04 17:44:18 -0700

    init

---
+++ hello_world.py
@@ -0,0 +1,28 @@
+message = "Hello World of Pythong"
+print(message)
+message = "What"
+print(message)
+
+##STRING CONVENTIONS
+#string titling
+name = "ada lovElace titling"
+print(name.title())
+#string casing
+name = "ada LOVElace casing"
+print(name.upper() + "upper")
+print(name.lower() + "lower")
+#string concatenation
+first_name = "adadada"
+last_name = "lovelace"
+full_name = first_name+ " " + last_name
+print(full_name + "concat")
+#whitespaces
+print("Languages:\nPython\n\tC\n\t\tJavascript" + "whitespacing")
+#stripping white space 
+fav_lang = "\tpython is my favorite    langauge"
+print(fav_lang)
+fav_lang = fav_lang.strip()
+print(fav_lang)
+print ("Roosters", 100-25*3%4)
+
+
